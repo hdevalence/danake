133bd933ad4fabaa8618bbc1fb52395a5c9056bf
Henry de Valence <hdevalence@hdevalence.ca>
2020-01-20 17:11:06 -0800

    change benchmark to benchmark topup processing time

--- benches/danake.rs
+++ benches/danake.rs
@@ -1,4 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 
 use chrono;
 use merlin::Transcript;
@@ -7,8 +7,8 @@ use rand;
 use danake;
 
 // Only do one benchmark for now so we can move things around later.
-pub fn wallet_issuance_response(c: &mut Criterion) {
-    use danake::{wallet::*, Epoch, EpochParameters};
+pub fn wallet_topup_response(c: &mut Criterion) {
+    use danake::{wallet::*, EpochParameters};
 
     let epoch_params = EpochParameters::from(std::time::Duration::from_secs(86400));
     let epoch = epoch_params.epoch_at(chrono::Utc::now());
@@ -23,18 +23,39 @@ pub fn wallet_issuance_response(c: &mut Criterion) {
         rand::thread_rng(),
     );
 
-    c.bench_function("wallet issuance request", |b| {
+    let response = secret
+        .issue(
+            request,
+            Transcript::new(b"wallet issuance test"),
+            rand::thread_rng(),
+        )
+        .expect("issuance should succeed");
+
+    let wallet = client_state
+        .verify_response(response)
+        .expect("response should verify");
+
+    let (_client_state, request) = wallet
+        .request_topup(
+            20_000,
+            &params,
+            Transcript::new(b"wallet topup test"),
+            rand::thread_rng(),
+        )
+        .expect("epoch is correct");
+
+    c.bench_function("wallet topup request", |b| {
         b.iter(|| {
-            let response = secret
-                .issue(
+            let _response = secret
+                .topup(
                     request.clone(),
-                    Transcript::new(b"wallet issuance test"),
+                    Transcript::new(b"wallet topup test"),
                     rand::thread_rng(),
                 )
-                .expect("issuance should succeed");
+                .expect("topup should succeed");
         })
     });
 }
 
-criterion_group!(wallet_issuance, wallet_issuance_response);
+criterion_group!(wallet_issuance, wallet_topup_response);
 criterion_main!(wallet_issuance);
