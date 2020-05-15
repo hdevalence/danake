a484afc6b8a376bc1e7d883e73c61a87e9fce180
redshiftzero <jen@freedom.press>
2020-05-15 17:38:53 -0400

    test: add rollover step

--- tests/danake.rs
+++ tests/danake.rs
@@ -1,11 +1,10 @@
 use chrono;
 use merlin::Transcript;
 use rand;
-
 use danake;
 
 #[test]
-fn wallet_issuance_and_topup() {
+fn wallet_issuance_topup_and_rollover() {
     use danake::{wallet::*, EpochParameters};
 
     let epoch_params = EpochParameters::from(std::time::Duration::from_secs(86400));
@@ -50,7 +49,37 @@ fn wallet_issuance_and_topup() {
         )
         .expect("topup should succeed");
 
-    let _wallet2 = client_state
+    let wallet2 = client_state
+        .verify_response(response)
+        .expect("response should verify");
+
+    let new_epoch = epoch_params.epoch_at(chrono::Utc::now());
+    let new_secret = Secrets::new(new_epoch, rand::thread_rng());
+    let new_parameters = Parameters::from(&new_secret);
+
+    let (client_state, request) = wallet2
+    .request_rollover(
+        &params,
+        &new_parameters,
+        Transcript::new(b"wallet rollover test"),
+        rand::thread_rng(),
+    )
+    .expect("rollover request should succeed");
+
+    let nullifier_result = true;
+    let check_and_update_closure = |_x| {nullifier_result};
+
+    let response = request
+        .rollover(
+            secret,
+            new_secret,
+            Transcript::new(b"wallet rollover test"),
+            rand::thread_rng(),
+            check_and_update_closure,
+        )
+        .expect("rollover should succeed");
+
+    let _wallet3 = client_state
         .verify_response(response)
         .expect("response should verify");
 }
